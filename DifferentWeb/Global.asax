b1537cbbf5a812842f93de8f18de0b441cf081a2
Endrit Tmava <endrit.tmava@riinvest.net>
2020-06-08 17:08:51 +0200

    Add project files.

---
+++ DifferentWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DifferentWeb.MvcApplication" Language="C#" %>
---
+++ DifferentWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DifferentWeb.Startup))]
+namespace DifferentWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
